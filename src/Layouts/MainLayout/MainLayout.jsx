578559d6bd392ee738e441b8518e901c4d3f47c6
tushar <iktushar2020@gmail.com>
2025-08-02 22:38:28 +0600

    ScrollToTop

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../../Shared/NavBar/NavBar';
 import Footer from '../../Shared/Footer/Footer';
+import ScrollToTop from '../../Shared/ScrollToTop/ScrollToTop';
 
 const MainLayout = () => {
     return (
         <div>
+             <ScrollToTop />
             <NavBar/>
             <Outlet/>
             <Footer/>
